2e934b94adb9cc39997a1e3ea0250d34d7ce86c4
Sokaaa <Alex-Pqn@users.noreply.github.com>
2023-10-24 14:40:31 +0200

    Add Error page, add Intro and Discover sections to Home page, styles fixs

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,8 +1,21 @@
-// import styles from './Error.module.scss'
-import React from 'react'
+import styles from './Error.module.scss'
+import { Helmet } from 'react-helmet'
+import DefaultHero from '@/components/Default/DefaultHero/DefaultHero'
 
 const Error = (): React.JSX.Element => {
-  return <div>Error</div>
+  return (
+    <div className={styles.container}>
+      {/* Head */}
+      <Helmet>
+        <title>Page introuvable • Alexandre Paquien</title>
+      </Helmet>
+      {/* DefaultHero */}
+      <DefaultHero
+        title={'Page introuvable.'}
+        description={"La page que vous avez demandé n'existe pas ou plus"}
+      />
+    </div>
+  )
 }
 
 export default Error
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import styles from './Home.module.scss'
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 import DefaultHero from '@/components/Default/DefaultHero/DefaultHero'
+import Photo from '@/assets/photo.jpg'
+import IconArrowRight from '@/components/Icon/IconArrowRight/IconArrowRight'
 
 const Home = (): React.JSX.Element => {
   return (
@@ -9,17 +11,28 @@ const Home = (): React.JSX.Element => {
       <DefaultHero title={"Je m'appelle Alexandre."} />
       {/* Content */}
       <section className={styles.content}>
-        <div className={styles.content__left}>
-          <img src="" alt="" />
+        {/* Intro */}
+        <div className={styles.intro}>
+          <div className={styles.intro__photo}>
+            <img src={Photo} width="350px" height="450px" alt="Photo" />
+          </div>
+          <div className={styles.intro__text}>
+            <h2>
+              Je suis un Développeur Web motivé, à la recherche de son premier
+              emploi dans le secteur.
+            </h2>
+            <p>
+              Après trois années de formation en développement web, mon objectif
+              est désormais d'intégrer une équipe dynamique où je pourrai mettre
+              en pratique mes compétences acquises au cours de ces dernières
+              années.
+            </p>
+          </div>
         </div>
-        <div className={styles.content__right}>
-          <h2>Je suis Développeur Web</h2>
-          <p>
-            Passionné par l’informatique depuis de nombreuses années, je débute
-            le développement web dans ma chambre, en autodidacte. Je décide
-            finalement de quitter les études secondaires début 2020 pour me
-            consacrer pleinement à cet univers.
-          </p>
+        <div className={styles.discover}>
+          <NavLink to={'/portfolio'}>
+            Découvrir mon portfolio <IconArrowRight size="27px" />
+          </NavLink>
         </div>
       </section>
     </div>

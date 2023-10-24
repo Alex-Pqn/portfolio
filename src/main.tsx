f4cbad82999234edffbdef24a0fc44663a1a5ca1
Sokaaa <Alex-Pqn@users.noreply.github.com>
2023-10-24 14:38:05 +0200

    Router: remove "Contact" page, add "Error" page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import Root from './routes/root'
-// import Home from './pages/Home/Home'
+import Home from './pages/Home/Home'
 import Portfolio from './pages/Portfolio/Portfolio'
 import Cv from './pages/Cv/Cv'
-import Contact from './pages/Contact/Contact'
+// import Contact from './pages/Contact/Contact'
 import Error from './pages/Error/Error'
-import Maintenance from './pages/Maintenance/Maintenance'
 
 const router = createBrowserRouter([
   {
@@ -16,13 +15,13 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <Error />,
     children: [
-      // {
-      //   path: '',
-      //   element: <Home />,
-      // },
+      {
+        path: '*',
+        element: <Error />,
+      },
       {
         path: '',
-        element: <Maintenance />,
+        element: <Home />,
       },
       {
         path: '/portfolio',
@@ -32,10 +31,10 @@ const router = createBrowserRouter([
         path: '/cv',
         element: <Cv />,
       },
-      {
-        path: '/contact',
-        element: <Contact />,
-      },
+      // {
+      //   path: '/contact',
+      //   element: <Contact />,
+      // },
     ],
   },
 ])

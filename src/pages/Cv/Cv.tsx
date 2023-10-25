44c2cf0e355f061d40e4bc2ba06f060c71cddff4
Sokaaa <Alex-Pqn@users.noreply.github.com>
2023-10-25 12:56:23 +0200

    Replace CV import with public url, add a file name on download

--- src/pages/Cv/Cv.tsx
+++ src/pages/Cv/Cv.tsx
@@ -2,10 +2,11 @@ import styles from './Cv.module.scss'
 import { Helmet } from 'react-helmet-async'
 import DefaultHero from '@/components/Default/DefaultHero/DefaultHero'
 import DocumentPdf from '@/components/Document/DocumentPdf/DocumentPdf'
-import CV from '@/assets/CV - Alexandre Paquien.pdf'
 import IconDownload from '@/components/Icon/IconDownload/IconDownload'
 
 const Cv = (): React.JSX.Element => {
+  const cvFilePath = './CV_Alexandre_Paquien.pdf'
+
   return (
     <>
       {/* Head */}
@@ -19,14 +20,18 @@ const Cv = (): React.JSX.Element => {
         {/* Content */}
         <section className={styles.content}>
           <div className={styles['content__download-cv']}>
-            <a href={CV} download>
+            <a
+              href={cvFilePath}
+              target="_blank"
+              download="CV_Alexandre_Paquien.pdf"
+            >
               <button>
-                Télécharger <IconDownload />
+                Télécharger <IconDownload size="18px" />
               </button>
             </a>
           </div>
           <div className={styles.content__cv}>
-            <DocumentPdf file={CV} />
+            <DocumentPdf file={cvFilePath} />
           </div>
         </section>
       </div>

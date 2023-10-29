d5006f45232ae200a58589eeb22a7044bff86527
Sokaaa <Alex-Pqn@users.noreply.github.com>
2023-10-29 15:28:56 +0100

    Return to top on page load

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,19 @@
 import '@/styles/globals.scss'
 import './root.scss'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import DefaultHeader from '@/components/Default/DefaultHeader/DefaultHeader'
 import DefaultFooter from '@/components/Default/DefaultFooter/DefaultFooter'
 
 const Root = (): React.JSX.Element => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       {/* Head */}

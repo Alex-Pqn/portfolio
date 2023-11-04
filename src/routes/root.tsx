c88caaf8fedd96de17f7a3c3e4d049b33dfaef51
Sokaaa <Alex-Pqn@users.noreply.github.com>
2023-11-04 18:45:40 +0100

    Remove unused window.scrollTo function

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,17 +1,12 @@
 import '@/styles/globals.scss'
 import './root.scss'
 
-import React, { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { Outlet, ScrollRestoration } from 'react-router-dom'
 import DefaultHeader from '@/components/Default/DefaultHeader/DefaultHeader'
 import DefaultFooter from '@/components/Default/DefaultFooter/DefaultFooter'
 
 const Root = (): React.JSX.Element => {
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
   return (
     <>
       {/* Head */}
@@ -25,7 +20,7 @@ const Root = (): React.JSX.Element => {
         <DefaultHeader />
         <main>
           <Outlet />
-          <ScrollRestoration />
+          <ScrollRestoration  />
         </main>
         <DefaultFooter />
       </div>

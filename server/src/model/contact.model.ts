3e1c4f2ef573f7f6e961ac072a6d07d8165f6aa2
Sokaaa <Alex-Pqn@users.noreply.github.com>
2023-11-16 23:33:37 +0100

    Add sanitize-html with Joi

--- server/src/model/contact.model.ts
+++ server/src/model/contact.model.ts
@@ -1,16 +1,37 @@
 import Joi from 'joi'
 import tlds from 'tlds'
+import sanitizeHtml from 'sanitize-html'
 
-export const contactSchema = Joi.object({
-  name: Joi.string().min(5).max(32).required().label('name'),
-  phone: Joi.string()
+const customJoi = Joi.extend((joi) => {
+  return {
+    type: 'string',
+    base: joi.string(),
+    rules: {
+      htmlStrip: {
+        validate(value) {
+          return sanitizeHtml(value, {
+            allowedTags: [],
+            allowedAttributes: {},
+          })
+        },
+      },
+    },
+  }
+})
+
+export const contactSchema = customJoi.object({
+  name: customJoi.string().min(5).max(32).htmlStrip().required(),
+  phone: customJoi
+    .string()
     .min(5)
     .max(32)
+    .allow(null, '')
+    .htmlStrip()
     .pattern(
       /(?:([+]\d{1,4})[-.\s]?)?(?:[(](\d{1,3})[)][-.\s]?)?(\d{1,4})[-.\s]?(\d{1,4})[-.\s]?(\d{1,9})/
-    )
-    .label('Phone'),
-  email: Joi.string()
+    ),
+  email: customJoi
+    .string()
     .email({
       tlds: {
         allow: tlds,
@@ -18,8 +39,8 @@ export const contactSchema = Joi.object({
     })
     .min(5)
     .max(32)
-    .required()
-    .label('Email'),
-  subject: Joi.string().min(5).max(100).required().label('Subject'),
-  message: Joi.string().min(25).max(5000).required().label('Message'),
+    .htmlStrip()
+    .required(),
+  subject: customJoi.string().min(5).max(100).htmlStrip().required(),
+  message: customJoi.string().min(25).max(5000).htmlStrip().required(),
 })
